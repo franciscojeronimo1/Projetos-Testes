17588b3f949fe6dcd25c7574c381cf509db25f09
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2023-05-10 23:34:06 -0300

    Update script.js

--- Relógio/js/script.js
+++ Relógio/js/script.js
@@ -1,6 +1,6 @@
 const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
-const segundos = document.getElementBy('segundos');
+const segundos = document.getElementById('segundos');
 
 const relogio = setInterval(function time() {
     let dateToday = new Date();
